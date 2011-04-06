80ad9ecfb8c75336f3380586644871c96521b8e5
Robert Escriva <me@robescriva.com>
2011-04-06 08:53:06 -0400

    The basic error class and a test runner.

---
+++ include/error.h
@@ -0,0 +1,98 @@
+// Copyright (c) 2011, Robert Escriva
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright notice,
+//     this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of po6 nor the names of its contributors may be used
+//       to endorse or promote products derived from this software without
+//       specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef po6_error_h_
+#define po6_error_h_
+
+// C
+#include <cstdlib>
+
+// C++
+#include <exception>
+#include <stdexcept>
+
+namespace po6
+{
+
+// Check if it is safe to throw an exception.  If so, throw a std::logic error.
+// If not, print the error message to stderr and abort.
+inline void
+logic_error(const std::string& message)
+{
+    // If we are already processing an exception.
+    if (std::uncaught_exception())
+    {
+        std::cerr << message << std::endl;
+        abort();
+    }
+    else
+    {
+        throw std::logic_error(message);
+    }
+}
+
+class error : public std::exception
+{
+    public:
+        error()
+            : m_errno(0)
+        {
+        }
+
+        error(int num)
+            : m_errno(num)
+        {
+        }
+
+        error(const error& e)
+            : m_errno(e.m_errno)
+        {
+        }
+
+        ~error() throw ()
+        {
+        }
+
+    public:
+        operator int () const
+        {
+            return m_errno;
+        }
+
+        error& operator = (const error& rhs)
+        {
+            m_errno = rhs.m_errno;
+            return *this;
+        }
+
+    private:
+        int m_errno;
+};
+
+} // namespace po6
+
+#endif // po6_error_h_
