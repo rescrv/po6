d54172411724e944baf1e2cf1f646760f30d31fc
Robert Escriva <me@robescriva.com>
2011-04-13 08:15:57 -0400

    Add ip address type.

---
+++ include/po6/net/ipaddr.h
@@ -0,0 +1,257 @@
+// Copyright (c) 2011, Robert Escriva
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright notice,
+//     this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of po6 nor the names of its contributors may be used
+//       to endorse or promote products derived from this software without
+//       specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef po6_net_ipaddr_h_
+#define po6_net_ipaddr_h_
+
+// POSIX
+#include <arpa/inet.h>
+#include <errno.h>
+#include <sys/socket.h>
+
+// po6
+#include <po6/error.h>
+
+namespace po6
+{
+namespace net
+{
+
+class ipaddr
+{
+    public:
+        ipaddr()
+            : m_family(AF_UNSPEC)
+            , m_ip()
+        {
+        }
+
+        ipaddr(const char* addr)
+            : m_family(AF_UNSPEC)
+            , m_ip()
+        {
+            set(addr);
+        }
+
+        ipaddr(const in_addr& ipv4)
+            : m_family(AF_UNSPEC)
+            , m_ip()
+        {
+            set(ipv4);
+        }
+
+        ipaddr(const in6_addr& ipv6)
+            : m_family(AF_UNSPEC)
+            , m_ip()
+        {
+            set(ipv6);
+        }
+
+        ipaddr(const ipaddr& other)
+            : m_family(other.m_family)
+            , m_ip(other.m_ip)
+        {
+        }
+
+        ~ipaddr()
+        {
+        }
+
+    public:
+        int family() const
+        {
+            return m_family;
+        }
+
+        void pack(struct sockaddr* addr, socklen_t* addrlen, in_port_t port) const
+        {
+            memset(addr, 0, *addrlen);
+
+            if (m_family == AF_UNSPEC)
+            {
+                throw std::logic_error("cannot pack a sockaddr with AF_UNSPEC");
+            }
+            else if (m_family == AF_INET && *addrlen >= sizeof(sockaddr_in))
+            {
+                sockaddr_in* addr4 = reinterpret_cast<sockaddr_in*>(addr);
+                addr4->sin_family = AF_INET;
+                addr4->sin_port = htons(port);
+                memmove(&addr4->sin_addr, &m_ip.v4, sizeof(in_addr));
+                *addrlen = sizeof(sockaddr_in);
+            }
+            else if (m_family == AF_INET6 && *addrlen >= sizeof(sockaddr_in6))
+            {
+                sockaddr_in6* addr6 = reinterpret_cast<sockaddr_in6*>(addr);
+                addr6->sin6_family = AF_INET6;
+                addr6->sin6_port = htons(port);
+                memmove(&addr6->sin6_addr, &m_ip.v6, sizeof(in6_addr));
+                *addrlen = sizeof(sockaddr_in6);
+            }
+            else
+            {
+                throw std::length_error("insufficiently sized sockaddr");
+            }
+        }
+
+        int compare(const ipaddr& rhs) const
+        {
+            const ipaddr& lhs(*this);
+
+            if (lhs.m_family != rhs.m_family)
+            {
+                return lhs.m_family - rhs.m_family;
+            }
+
+            if (lhs.m_family == AF_INET)
+            {
+                return memcmp(&lhs.m_ip.v4, &rhs.m_ip.v4, sizeof(in_addr));
+            }
+            else if (lhs.m_family == AF_INET6)
+            {
+                return memcmp(&lhs.m_ip.v6, &rhs.m_ip.v6, sizeof(in6_addr));
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+    public:
+        void set(const char* addr)
+        {
+            in_addr ipv4;
+            in6_addr ipv6;
+
+            if (inet_pton(AF_INET, addr, &ipv4) > 0)
+            {
+                set(ipv4);
+            }
+            else if (inet_pton(AF_INET6, addr, &ipv6) > 0)
+            {
+                set(ipv6);
+            }
+            else
+            {
+                if (errno != 0)
+                {
+                    throw po6::error(errno);
+                }
+                else
+                {
+                    throw std::invalid_argument("malformed address");
+                }
+            }
+        }
+
+        void set(const in_addr& ipv4)
+        {
+            m_family = AF_INET;
+            memmove(&m_ip.v4, &ipv4, sizeof(in_addr));
+        }
+
+        void set(const in6_addr& ipv6)
+        {
+            m_family = AF_INET6;
+            memmove(&m_ip.v6, &ipv6, sizeof(in6_addr));
+        }
+
+    public:
+        ipaddr& operator = (const ipaddr& rhs)
+        {
+            if (this != &rhs)
+            {
+                m_family = rhs.m_family;
+                memmove(&m_ip, &rhs.m_ip, sizeof(m_ip));
+            }
+
+            return *this;
+        }
+
+        bool operator < (const ipaddr& rhs) const
+        {
+            return compare(rhs) < 0;
+        }
+
+        bool operator == (const ipaddr& rhs) const
+        {
+            return compare(rhs) == 0;
+        }
+
+        bool operator != (const ipaddr& rhs) const
+        {
+            return compare(rhs) != 0;
+        }
+
+    private:
+        friend std::ostream& operator << (std::ostream& lhs, const ipaddr& rhs);
+
+    private:
+        int m_family;
+        union
+        {
+            in_addr v4;
+            in6_addr v6;
+        } m_ip;
+};
+
+inline std::ostream&
+operator << (std::ostream& lhs, const ipaddr& rhs)
+{
+    if (rhs.m_family == AF_UNSPEC)
+    {
+        lhs << "UNSPECIFIED";
+    }
+    else if (rhs.m_family == AF_INET)
+    {
+        char repr[INET_ADDRSTRLEN];
+
+        if (!inet_ntop(AF_INET, &rhs.m_ip.v4, repr, INET_ADDRSTRLEN))
+        {
+            po6::logic_error("inet_ntop failed, but should never fail.");
+        }
+
+        lhs << std::string(repr);
+    }
+    else if (rhs.m_family == AF_INET6)
+    {
+        char repr[INET6_ADDRSTRLEN];
+
+        if (!inet_ntop(AF_INET6, &rhs.m_ip.v6, repr, INET6_ADDRSTRLEN))
+        {
+            po6::logic_error("inet_ntop failed, but should never fail.");
+        }
+
+        lhs << std::string(repr);
+    }
+
+    return lhs;
+}
+
+} // namespace net
+} // namespace po6
+
+#endif // po6_net_ipaddr_h_
---
+++ test/net/ipaddr.cc
@@ -0,0 +1,118 @@
+// Copyright (c) 2011, Robert Escriva
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright notice,
+//     this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of po6 nor the names of its contributors may be used
+//       to endorse or promote products derived from this software without
+//       specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+// Google Test
+#include <gtest/gtest.h>
+
+// po6
+#include "po6/net/ipaddr.h"
+
+#pragma GCC diagnostic ignored "-Wswitch-default"
+
+namespace
+{
+
+TEST(IpaddrTest, CreateAndCompare)
+{
+    in_addr ipv4;
+    in6_addr ipv6;
+
+    ASSERT_EQ(inet_pton(AF_INET, "127.0.0.1", &ipv4), 1);
+    ASSERT_EQ(inet_pton(AF_INET6, "::1", &ipv6), 1);
+
+    po6::net::ipaddr a;
+    po6::net::ipaddr b("127.0.0.1");
+    po6::net::ipaddr c("::1");
+    po6::net::ipaddr d(ipv4);
+    po6::net::ipaddr e(ipv6);
+    po6::net::ipaddr aprime(a);
+    po6::net::ipaddr bprime(b);
+    po6::net::ipaddr cprime(c);
+    po6::net::ipaddr dprime(d);
+    po6::net::ipaddr eprime(e);
+
+    EXPECT_EQ(a, aprime);
+    EXPECT_EQ(b, bprime);
+    EXPECT_EQ(c, cprime);
+    EXPECT_EQ(d, dprime);
+    EXPECT_EQ(e, eprime);
+
+    EXPECT_NE(a, b);
+    EXPECT_NE(b, c);
+    EXPECT_NE(c, d);
+    EXPECT_NE(d, e);
+}
+
+TEST(IpaddrTest, Assignment)
+{
+    po6::net::ipaddr lhs;
+    po6::net::ipaddr rhs("127.0.0.1");
+
+    EXPECT_NE(lhs, rhs);
+    lhs = rhs;
+    EXPECT_EQ(lhs, rhs);
+}
+
+TEST(IpaddrTest, SelfAssignment)
+{
+    po6::net::ipaddr ip("127.0.0.1");
+
+    EXPECT_EQ(ip, po6::net::ipaddr("127.0.0.1"));
+    EXPECT_EQ(ip = ip, po6::net::ipaddr("127.0.0.1"));
+    EXPECT_EQ(ip, po6::net::ipaddr("127.0.0.1"));
+}
+
+TEST(IpaddrTest, PackV4)
+{
+    sockaddr sa;
+    socklen_t sz = sizeof(sa);
+    po6::net::ipaddr ip("127.0.0.1");
+
+    EXPECT_EQ(ip.family(), AF_INET);
+    ip.pack(&sa, &sz, 1234);
+    EXPECT_EQ(reinterpret_cast<sockaddr_in*>(&sa)->sin_family, AF_INET);
+    EXPECT_EQ(reinterpret_cast<sockaddr_in*>(&sa)->sin_addr.s_addr, htonl(0x7f000001));
+    EXPECT_EQ(reinterpret_cast<sockaddr_in*>(&sa)->sin_port, htons(1234));
+}
+
+TEST(IpaddrTest, PackV6)
+{
+    sockaddr_in6 sa;
+    socklen_t sz = sizeof(sa);
+    po6::net::ipaddr ip("::1");
+    in6_addr ipv6;
+
+    ASSERT_EQ(inet_pton(AF_INET6, "::1", &ipv6), 1);
+
+    EXPECT_EQ(ip.family(), AF_INET6);
+    ip.pack(reinterpret_cast<sockaddr*>(&sa), &sz, 1234);
+    EXPECT_EQ(sa.sin6_family, AF_INET6);
+    EXPECT_EQ(sa.sin6_port, htons(1234));
+    EXPECT_EQ(memcmp(&sa.sin6_addr, &ipv6, sizeof(in6_addr)), 0);
+}
+
+} // namespace
